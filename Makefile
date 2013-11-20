6c40c33862dae076f4ce43d25ef1d89d0596337b
Ronald Chan <ronalchn@gmail.com>
2013-11-20 22:28:39 +1300

    remove executable, add bin dir, add log file for closing metafile

--- Makefile
+++ Makefile
@@ -1,6 +1,10 @@
 all: metafile-writer
 
-metafile-writer: metafile-writer/main
+metafile-writer: bin/metafile-writer
 
-metafile-writer/main: metafile-writer/main.cpp
-	g++ -o metafile-writer/main metafile-writer/main.cpp
+bin/metafile-writer: bin metafile-writer/main.cpp
+	g++ -o bin/metafile-writer metafile-writer/main.cpp
+
+bin:
+	mkdir bin -p
+
--- metafile-writer/main.cpp
+++ metafile-writer/main.cpp
@@ -7,13 +7,15 @@ int main() {
   cout << "Hello World!" << endl;
 
   FILE *metafile = fdopen(3,"w");
-  fprintf(metafile,">> START metafile-writer <<\n");
-  fprintf(metafile,"time:0.000\n");
-  fprintf(metafile,"cg-mem:0\n");
-  fprintf(metafile,"exitcode:0\n");
-  fprintf(metafile,"status:XX\n");
-  fprintf(metafile,"message:Out of memory\n");
-  fprintf(metafile,">> END metafile-writer <<\n");
+  if (metafile != NULL) {
+    fprintf(metafile,">> START metafile-writer <<\n");
+    fprintf(metafile,"time:0.000\n");
+    fprintf(metafile,"cg-mem:0\n");
+    fprintf(metafile,"exitcode:0\n");
+    fprintf(metafile,"status:XX\n");
+    fprintf(metafile,"message:Out of memory\n");
+    fprintf(metafile,">> END metafile-writer <<\n");
+  }
 
   return 0;
 }
